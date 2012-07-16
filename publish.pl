dea0fa855acb1d0b5116a5d0c1d7ad423f7f5365
Matthias Ferber <matthias@matthiasferber.net>
2012-07-15 23:31:08 -0400

    Added pdf-calendar handling, to generate a PDF of an arbitrary calendary range (for adding new pages to a previously-printed calendar without any interruption between pages).  In the process, recast the command line interface to handle multiple phases, specified without -p; related refactorings.  Added usage comments at top, removed previous version of -h message.

--- publish.pl
+++ publish.pl
@@ -8,6 +8,51 @@
 # - Generating HTML for web browser viewing;
 # - Generating PDFs for printing.
 #
+# Command line format:
+#   publish.pl [options...] [specs...] <month-range>
+#   publish.pl [options...] pdf-calendar <date-range> <outfile>
+#
+# Options:
+#   -g   Debug mode: produces additional output
+#   -h   Print help menu
+#
+# Specs:
+# 
+#   Any combination of the following output specifiers, indicating
+#   which publication phases should be run.  If none of these are
+#   listed on the command line, all phases are run.
+#     xml - process all input files into the monthly XML files (the
+#       documents of record)
+#     html - generate monthly and yearly HTML files for browsing
+#     pdf - generate monthly and yearly PDF files for printing
+#
+#   The special specifier "pdf-calendar" may be used to generate a
+#   PDF calendar covering an arbitrary date range.  This feature is
+#   there to support keeping a continuous calendar that new pages
+#   can be added to over time without interrupting the content flow.
+#
+# Range:
+#
+#   The date range to process.  May be a single month (M/YY), a
+#   range of months (M/YY-M/YY), or "all" to process all available
+#   dates.
+#
+#   For "pdf-calendar" mode only, must be a date range given as
+#   M/D/YY-M/D/YY.  The XML must have been generated for the months
+#   in that range already.  The output file must also be specified
+#   on the command line.
+#
+# Command line examples:
+#
+#   publish.pl 1/01
+#   publish.pl 1/01-7/10
+#   publish.pl pdf 1/01
+#   publish.pl xml html 2/01
+#   publish.pl all
+#   publish.pl html all
+#
+#   publish.pl pdf-calendar 1/5/06-8/13/09
+#
 ######################################################################
 
 BEGIN {
@@ -55,24 +100,108 @@ our $FOP_OPTS = '-Xmx1024m '
             . 'net.sf.saxon.TransformerFactoryImpl '
         . '-Djava.awt.headless=true';
         
+
+
+our ($from_mo, $from_yr, $to_mo, $to_yr);
+our ($pdf_calendar_start, $pdf_calendar_end, $pdf_calendar_outfile);
+our %phases;
 our %opts;
-getopt('mftp', \%opts);
+
+getopt('', \%opts);
+
+print_help_and_exit() if $opts{h};
+our $debug = $opts{g};
+
+# Determine what phases to run
+while (@ARGV and $ARGV[0] =~ /^(xml|html|pdf|pdf-calendar)$/) {
+    $phases{$ARGV[0]} = 1;
+    shift;
+}
+if ( !(keys(%phases)) ) {       # no specified phases = use all
+    $phases{xml} = 1;
+    $phases{html} = 1;
+    $phases{pdf} = 1;
+}
+if ($phases{'pdf-calendar'} and scalar(keys(%phases)) != 1) {
+    die "'pdf-calendar' phase cannot be used with other phases.\n\n";
+    print_help_and_exit();
+}
+
+# Get time range to process
+if ($phases{'pdf-calendar'}) {
+    ($pdf_calendar_start, $pdf_calendar_end) =
+        get_pdf_calendar_dates($ARGV[0]);
+    $pdf_calendar_outfile = $ARGV[1];
+    unless ( defined($pdf_calendar_start) ) {
+        print "Error: PDF calendar date range missing or incomplete.\n\n";
+        print_help_and_exit();
+    }
+    unless ( defined($pdf_calendar_outfile) ) {
+        print "Error: PDF calendar output file path missing.\n\n";
+        print_help_and_exit();
+    }
+}
+else {
+    # "normal" phases: get range of months
+    ($from_mo, $from_yr, $to_mo, $to_yr) = get_month_range($ARGV[0]);
+    unless (defined($from_mo)) {
+        print "Error: range of months missing.\n\n";
+        print_help_and_exit();
+    }
+    
+    # Validate and normalize dates
+    if ($from_mo < 1 || $from_mo > 12) {
+        die "Invalid starting month";
+    }
+    if ($to_mo < 1 || $to_mo > 12) {
+        die "Invalid ending month";
+    }
+    $from_yr += 2000 if $from_yr < 100;
+    $to_yr += 2000 if $to_yr < 100;
+}
+
+if ($phases{'pdf-calendar'}) {
+    process_arbitrary_pdf_calendar($pdf_calendar_start,
+        $pdf_calendar_end, $pdf_calendar_outfile);
+}
+else {
+    process_months(\%phases, $from_mo, $from_yr, $to_mo, $to_yr);
+}
+print "Done.\n";
+
+
+
 
 #
-# Get time range to process
+# Get the month/year range from the command line argument (for all
+# phases except pdf-calendar
 #
-my ($from_mo, $from_yr, $to_mo, $to_yr);
-if (@ARGV) {
-    if ($ARGV[0] eq 'all') {
+sub get_month_range
+{
+    my $arg = shift;
+    defined($arg) or return (undef, undef, undef, undef);
+    
+    my ($from_mo, $from_yr, $to_mo, $to_yr);
+    if ($arg eq 'all') {
         ($from_mo, $from_yr, $to_mo, $to_yr) =
-            get_all_range();
+            get_all_months_range();
         if ( defined($from_mo) ) {
             print "Processing complete range: ",
                 "$from_mo/$from_yr-$to_mo/$to_yr\n"
         }
     }
     else {
-        if ($ARGV[0] =~ m|^(\d{1,2})/(\d{2}(?:\d{2})?)(?:-(\d{1,2})/(\d{2}(?:\d{2})?))?$|) {
+        if ($arg =~
+            m|
+                ^                               # starting month...
+                (\d{1,2})/(\d{2}(?:\d{2})?)     #   M/YY or M/YYYY
+                (?:                             # ending month...
+                    -
+                    (\d{1,2})/(\d{2}(?:\d{2})?) # M/YY or M/YYYY
+                )?                              # ending month is optional
+                $
+            |x
+        ) {
             ($from_mo, $from_yr) = ($1, $2);
             if (defined($3)) {
                 ($to_mo, $to_yr) = ($3, $4);
@@ -82,57 +211,15 @@ if (@ARGV) {
             }
         }
     }
+    return ($from_mo, $from_yr, $to_mo, $to_yr);
 }
 
-# Usage
-if ($opts{h} || !defined($from_mo)) {
-    my $scriptname = basename($0);
-    print << "END_USAGE";
-$scriptname M/YY-M/YY
-$scriptname M/YY
-$scriptname all
-
-Optional argument: -p <phase> where <phase> is: xml|html|pdf.  If
--p is omitted, all phases will be run.
-
-Generates monthly XML journal files for all entries in the specified
-range.
-
-Other arguments:
-  -g   Output debugging messages
-  
-XML output:
-$XML_DIR
-
-HTML output:
-$HTML_DIR
-
-PDF output:
-$PDF_DIR
-
-M/YY    month/year to process; M/YY-M/YY for a range
-
-END_USAGE
-    exit 0;
-}
-our $phase = $opts{p};
-our $debug = $opts{g};
-
-# Validate and normalize dates
-if ($from_mo < 1 || $from_mo > 12) {
-    die "Invalid starting month";
-}
-if ($to_mo < 1 || $to_mo > 12) {
-    die "Invalid ending month";
-}
-$from_yr += 2000 if $from_yr < 100;
-$to_yr += 2000 if $to_yr < 100;
 
 #
 # Get the month/year range when the user specified "all"
 # returns (from_mo, from_yr, to_mo, to_yr)
 #
-sub get_all_range
+sub get_all_months_range
 {
     my $pat = catfile($JOURNAL_BASE, 'journal.????-??-??.txt');
     my @entries = sort(glob($pat));
@@ -160,26 +247,48 @@ sub get_all_range
 
 
 #
-# Process months
+# Get the date range for arbitrary PDF calendar generation
 #
-if (!defined($from_mo)) {
-    ($from_mo, $from_yr) = (1, 2000);
-}
-if (!defined($to_mo)) {
-    ($to_mo, $to_yr) = (12, 2100);
+sub get_pdf_calendar_dates
+{
+    my $arg = shift;
+    if ($arg =~
+        m|
+            ^
+            (\d{1,2})/(\d{1,2})/(\d{2}(?:\d{2})?)   # M/D/YY or M/D/YYYY
+            -
+            (\d{1,2})/(\d{1,2})/(\d{2}(?:\d{2})?)   # same
+            $
+        |x
+    ) {
+        my $y0 = (length($3) > 2 ? $3 : 2000 + $3);
+        my $y1 = (length($6) > 2 ? $6 : 2000 + $6);
+        my $start = sprintf("%04d-%02d-%02d", $y0, $1, $2);
+        my $end = sprintf("%04d-%02d-%02d", $y1, $4, $5);
+        return ($start, $end);
+    }
+    return (undef, undef);
 }
 
-for (my $yr = $from_yr; $yr <= $to_yr; ++$yr) {
-    my $mo0 = ($yr == $from_yr ? $from_mo : 1);
-    my $mo1 = ($yr == $to_yr ? $to_mo : 12);
-    if ($mo0 <= $mo1) {
-        for (my $mo = $mo0; $mo <= $mo1; ++$mo) {
-            process_month($mo, $yr);
+
+#
+# Process months
+#
+sub process_months
+{
+    my ($hr_phases, $from_mo, $from_yr, $to_mo, $to_year) = @_;
+
+    for (my $yr = $from_yr; $yr <= $to_yr; ++$yr) {
+        my $mo0 = ($yr == $from_yr ? $from_mo : 1);
+        my $mo1 = ($yr == $to_yr ? $to_mo : 12);
+        if ($mo0 <= $mo1) {
+            for (my $mo = $mo0; $mo <= $mo1; ++$mo) {
+                process_month($hr_phases, $mo, $yr);
+            }
+            process_year($hr_phases, $yr);
         }
-        process_year($yr);
     }
 }
-print "Done.\n";
 
 
 #
@@ -187,15 +296,15 @@ print "Done.\n";
 #
 sub process_month
 {
-    my ($mo, $yr) = @_;
+    my ($hr_phases, $mo, $yr) = @_;
     
-    if (!defined($phase) or $phase eq 'xml') {
+    if ($hr_phases->{'xml'}) {
         collect_month_xml($mo, $yr);
     }
-    if (!defined($phase) or $phase eq 'html') {
+    if ($hr_phases->{'html'}) {
         process_month_to_html($mo, $yr);
     }
-    if (!defined($phase) or $phase eq 'pdf') {
+    if ($hr_phases->{'pdf'}) {
         process_month_to_pdf($mo, $yr);
     }
 }
@@ -206,19 +315,19 @@ sub process_month
 #
 sub process_year
 {
-    my $yr = shift;
+    my ($hr_phases, $yr) = @_;
     
-    if (!defined($phase) or $phase eq 'html') {
+    if ($phases{'html'}) {
         process_year_to_html($yr);
     }
-    if (!defined($phase) or $phase eq 'pdf') {
+    if ($phases{'pdf'}) {
         process_year_to_pdf($yr);
     }
 }
 
 
 #
-# Subroutine: process a month of text to XML; returns the number of entries
+# Process a month of text to XML; returns the number of entries
 # found
 #
 sub collect_month_xml
@@ -286,7 +395,7 @@ sub collect_month_xml
 
 
 #
-# Subroutine: convert an entry to XML
+# Convert an entry to XML
 #
 sub convert_entry_to_xml
 {
@@ -347,7 +456,7 @@ sub convert_entry_to_xml
 
 
 #
-# Subroutine: tag italicized sections marked with *
+# Tag italicized sections marked with *
 # (this might be a good feature to replace with Markdown or MultiMarkdown)
 #
 sub write_with_tagged_italics {
@@ -365,7 +474,7 @@ sub write_with_tagged_italics {
 
 
 #
-# Subroutine: process a month from XML to HTML
+# Process a month from XML to HTML
 #
 sub process_month_to_html
 {
@@ -494,7 +603,34 @@ sub process_year_to_pdf
 
 
 #
-# Subroutine: Parse the entry date from the filename, and if provided with
+# Generate the PDF calendar for an arbitrary date range.  Pass in dates
+# formatted as YYYY-MM-DD.
+#
+sub process_arbitrary_pdf_calendar
+{
+    my ($startdate, $enddate, $outfile) = @_;
+    
+    print "Rendering calendar range to PDF...\n";
+    
+    my @cmd = ("$FOP_DIR/fop",
+        '-c', "$CONF_DIR/fop.xconf",
+        '-xml', "$LIB_DIR/xslt/null.xml",
+        '-xsl', "$LIB_DIR/xslt/year2fo.xsl",
+        '-pdf', $outfile,
+        '-param', 'input-base-uri', "file:$XML_DIR/",
+        '-param', 'start', $startdate,
+        '-param', 'end', $enddate
+    );
+    
+    print "\n", join(' ', @cmd), "\n\n" if $debug;
+    
+    $ENV{'FOP_OPTS'} = $FOP_OPTS;
+    system(@cmd);
+}
+
+
+#
+# Parse the entry date from the filename, and if provided with
 # a dateline, check that they match and emit a warning if not; return the
 # entry date as a perl date
 #
@@ -544,7 +680,7 @@ sub get_validated_date
 }
 
 
-# Subroutine: run SmartyPants algorithm to smartify quotes and dashes, but
+# Run SmartyPants algorithm to smartify quotes and dashes, but
 # then transform entities back to the actual UTF-8 character, since
 # SmartyPants doesn't do that natively
 sub smartypants
@@ -557,4 +693,15 @@ sub smartypants
     $tmp =~ s/&#8221;/”/g;
     $tmp =~ s/&#8212;/—/g;  # em dash
     return $tmp;
+}
+
+
+# Print help and exit
+sub print_help_and_exit
+{
+    my $scriptname = basename($0);
+    print << "END_USAGE";
+HELP TEXT HERE
+END_USAGE
+    exit 0;
 }
